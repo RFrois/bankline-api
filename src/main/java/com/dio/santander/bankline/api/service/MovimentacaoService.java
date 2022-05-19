c46acd30254af3f749a16e53f3442d92ae8f2e8e
Rafael Frois <rafael.frois1@gmail.com>
2022-05-19 01:42:28 -0300

    Pausa 3

---
+++ src/main/java/com/dio/santander/bankline/api/service/MovimentacaoService.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bankline.api.service;
+
+import org.springframework.stereotype.Service;
+
+import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+
+@Service
+public class MovimentacaoService {
+	public void save(NovaMovimentacao novaMovimentacao) {
+		
+	}
+
+}
