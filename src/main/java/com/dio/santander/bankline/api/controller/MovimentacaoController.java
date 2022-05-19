e0e6ad0ed06249c292af8102fb8383e582207f13
Rafael Frois <rafael.frois1@gmail.com>
2022-05-19 01:44:57 -0300

    Pausa2

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -10,21 +10,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dio.santander.bankline.api.dto.NovoCorrentista;
-import com.dio.santander.bankline.api.model.Correntista;
-import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.model.Movimentacao;
+import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
 import com.dio.santander.bankline.api.service.CorrentistaService;
 
 @RestController
-@RequestMapping("/correntistas")
+@RequestMapping("/movimentacoes")
 public class MovimentacaoController {
 	@Autowired
-	private CorrentistaRepository repository;
+	private MovimentacaoRepository repository;
 	
 	@Autowired
 	private CorrentistaService service;
 	
 	@GetMapping
-	public List<Correntista> findAll(){
+	public List<Movimentacao> findAll(){
 		return repository.findAll();
 	}
 	
